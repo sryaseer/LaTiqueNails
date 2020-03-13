c0ebe4f0c00e0e407dd7cc32001c3015f88de67e
Chris4545 <C.ung3232@gmail.com>
2020-03-12 23:05:14 -0500

    Added basic stuff to be able to connect program to RDB in future.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,21 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    final String hostname = "";
+    final String dbName = "";
+    final String port = "3306";
+    final String username = "";
+    final String password = "";
+    final String AWS_URL = "jdbc:mysql://" + hostname + ":" + port + "/" + dbName + "?user=" + username + "&password=" + password;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }

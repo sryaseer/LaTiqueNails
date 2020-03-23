e45d2a030c03246c0c011e98e81d6ee3ebe50b55
Chris4545 <C.ung3232@gmail.com>
2020-03-23 12:45:33 -0500

    Added Class DatabaseOperator(reduces code redundancy for database connections) and the Class SceneChanger(Helps simplify scene changes). Added Scene transitions to Dashboard and Login Page in SceneChanger.java.

--- src/sample/SceneChanger.java
+++ src/sample/SceneChanger.java
@@ -12,8 +12,22 @@ import java.io.IOException;
 
 public class SceneChanger {
     public void ChangeScene(int sceneNumber, ActionEvent event) throws IOException {
-        if(sceneNumber == 1){
+        if(sceneNumber == 1){ //transition to reports.fxml
             Parent mainWindowParent = FXMLLoader.load(getClass().getResource("Reports.fxml"));
+            Scene mainWindowScene = new Scene(mainWindowParent);
+            Stage window = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+            window.setScene(mainWindowScene);
+            window.show();
+        }
+        if(sceneNumber == 2){ //transitions to dashboard.fxml
+            Parent mainWindowParent = FXMLLoader.load(getClass().getResource("dashboard.fxml"));
+            Scene mainWindowScene = new Scene(mainWindowParent);
+            Stage window = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+            window.setScene(mainWindowScene);
+            window.show();
+        }
+        if(sceneNumber == 3){ //transitions to login page.fxml
+            Parent mainWindowParent = FXMLLoader.load(getClass().getResource("Login Page.fxml"));
             Scene mainWindowScene = new Scene(mainWindowParent);
             Stage window = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
             window.setScene(mainWindowScene);

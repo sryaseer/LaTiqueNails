0a4db6410a469d0c9ee247854c32c5eb0ba4f422
Chris4545 <C.ung3232@gmail.com>
2020-03-30 12:41:09 -0500

    Worked on AddCustomers scene

--- src/sample/AddCustomersController.java
+++ src/sample/AddCustomersController.java
@@ -1,13 +1,24 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class AddCustomersController extends DatabaseOperator implements Initializable {
+
+    @FXML
+    public TextField emailBox, phoneNumberBox, lastNameBox, firstNameBox;
+
+    @FXML
+    public Label invalidFirstName, invalidLastName, invalidPhone, invalidEmail;
+
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
@@ -16,4 +27,20 @@ public class AddCustomersController extends DatabaseOperator implements Initiali
     public void backButtonClicked(ActionEvent event) throws IOException {
         ChangeScene("dashboard.fxml",event);
     }
+
+    public void addClicked(ActionEvent event){
+        Validation.textFieldNotEmpty(firstNameBox,invalidFirstName,"Invalid");
+        Validation.textFieldNotEmpty(lastNameBox,invalidLastName,"Invalid");
+
+        if(Validation.textFieldNotEmpty(phoneNumberBox,invalidPhone,"Invalid")) //empty validation
+            Validation.numberFormat(phoneNumberBox,invalidPhone,"Invalid");     //has numbers validation
+
+        if (Validation.textFieldNotEmpty(emailBox,invalidEmail,"Invalid"))      //empty validation
+            Validation.emailFormat(emailBox,invalidEmail,"Invalid");            //is email validation
+
+
+    }
+
+
+
 }
